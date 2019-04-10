4399508e4c3c12f9aa1a91431ee49fd270c0c078
Viven <v.andzuana@mygento.ru>
2019-04-10 21:19:16 +0300

    suite corrected

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,6 +5,7 @@ import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.ITestContext;
 import org.testng.annotations.*;
 import org.testng.xml.XmlTest;
 
@@ -18,53 +19,54 @@ import static com.codeborne.selenide.Selenide.close;
  * {@link  LoginTest,}
  */
 public abstract class BaseTest {
-	protected Logger logger;
-	
-	@BeforeSuite
-	public void setUp() {
-		SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-		
-		Configuration.browser = "chrome";
-		Configuration.startMaximized = false;
+    protected Logger logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
+    @BeforeSuite
+    public void setUp(ITestContext context) {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = false;
 //		Configuration.timeout=4999;
-		Locale.setDefault(Locale.ENGLISH);
-		logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-		logger.info("Starting test suite");
-	}
-	
-	@BeforeMethod
-	public void setUpMethod(Method method)
-	{
-		logger.info("Starting test method: " + method.getName().toString());
-	}
-	
-	@AfterMethod
-	public void tearDownMethod(Method method)  {
-		logger.info("Exiting test method: " + method.getName().toString());
-		WebDriverRunner.getWebDriver().manage().deleteAllCookies();
-		
-	}
-	
-	@AfterSuite
-	public void tearDown() {
-		logger.info("Exiting test suite");
-		close();
-	}
-	@BeforeClass
-	public void setUpClass(XmlTest method)
-	{
-//		logger.info("Starting test class: " + method.getClass().toString());
-	}
-	@AfterClass
-	public void tearDownClass(XmlTest method)
-	{
-//		logger.info("Exiting test class: " + method.getClass().toString());
-	}
-	@AfterTest
-	public void clear() {
-	}
-	@Step
-	public void logAllure(String log){
-		logger.info("Logged to allure: " + log);
-	}
+        Locale.setDefault(Locale.ENGLISH);
+//        logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME); //TODO why npe
+
+        logger.info("Starting test suite " + context.getCurrentXmlTest().getSuite().getName());
+    }
+
+    @BeforeMethod
+    public void setUpMethod(Method method) {
+        logger.info("Starting test method: " + method.getName());
+    }
+
+    @AfterMethod
+    public void tearDownMethod(Method method) {
+        logger.info("Exiting test method: " + method.getName());
+        WebDriverRunner.getWebDriver().manage().deleteAllCookies();
+
+    }
+
+    @BeforeClass
+    public void setUpClass(ITestContext context) {
+        logger.info("Starting test class: " + context.getCurrentXmlTest().getXmlClasses().toString());
+    }
+
+    @AfterClass
+    public void tearDownClass(ITestContext context) {
+        logger.info("Exiting test class: " + context.getCurrentXmlTest().getXmlClasses().toString());
+    }
+
+    @AfterSuite
+    public void tearDown(ITestContext context) {
+        logger.info("Exiting test suite " + context.getCurrentXmlTest().getSuite().getName());
+        close();
+    }
+
+    //	@AfterTest
+//	public void clear() {
+//	}
+    @Step
+    public void logAllure(String log) {
+        logger.info("Logged to allure: " + log);
+    }
 }
